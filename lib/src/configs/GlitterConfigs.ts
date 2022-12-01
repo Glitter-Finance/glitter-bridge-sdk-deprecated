50b2ee4bf832ac7257162a82a4ef53eda8978b76
Andrew Carl <andrewc@glitter.finance>
2022-12-01 12:05:11 -0500

    ensured that config files build

--- lib/src/configs/GlitterConfigs.ts
+++ lib/src/configs/GlitterConfigs.ts
@@ -2,6 +2,9 @@ import { AlgorandConfig } from "glitter-bridge-algorand-dev";
 import { BridgeAccountConfig, BridgeTokenConfig } from "glitter-bridge-common-dev";
 import { SolanaConfig } from "glitter-bridge-solana-dev";
 
+import * as GlitterConfigs1 from './mainnet.settings.json';
+import * as GlitterConfigs2 from './testnet.settings.json';
+
 export enum GlitterNetworks {
     mainnet = "mainnet",
     testnet = "testnet",
